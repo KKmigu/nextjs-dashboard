97146de6aa66968a894d27d828bc7c6a3d7206bd
kkmigu <kkmigu5210@gmail.com>
2024-08-30 15:10:07 +0800

    study: Search and Pagination

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,39 @@
-export default function Page() {
-  return <p>Invoices Page</p>;
+import Pagination from '@/app/ui/invoices/pagination';
+import { fetchInvoicesPages } from '@/app/lib/data';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+import {CreateInvoice} from '@/app/ui/invoices/buttons';
+import {lusitana} from '@/app/ui/fonts';
+import {InvoicesTableSkeleton} from '@/app/ui/skeletons';
+import {Suspense} from 'react';
+// 在函数定义中，{searchParams}: { searchParams?: { query?: string; page?: string; }; }
+// 表示函数 Page 接受一个包含 searchParams 属性的对象，该属性是一个可选的对象，包含 query 和 page 属性，它们的值都是字符串类型。
+export default async function Page({searchParams,}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  console.log(searchParams)
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
+
+  return (
+      <div className="w-full">
+        <div className="flex w-full items-center justify-between">
+          <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+        </div>
+        <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+          <Search placeholder="Search invoices..."/>
+          <CreateInvoice/>
+        </div>
+        <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton/>}>
+          <Table query={query} currentPage={currentPage}/>
+        </Suspense>
+        <div className="mt-5 flex w-full justify-center">
+           <Pagination totalPages={totalPages} />
+        </div>
+      </div>
+  );
 }
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,16 +1,47 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import {usePathname, useRouter, useSearchParams} from 'next/navigation';
+import {useDebouncedCallback} from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
+
+  // 1.获取地址栏参数
+  const searchParams = useSearchParams();
+  // 2.读取当前 URL 的路径名
+  const pathname = usePathname();
+  // 3.用于路由替换
+  const { replace } = useRouter();
+
+  // useDebouncedCallback: 防抖函数  pnpm i use-debounce
+  const handleSearch = useDebouncedCallback((term: string) => {
+    // 4.将原本的地址栏参数构造到URLSearchParams对象
+    // URLSearchParams 是一个 Web API，它提供用于操作 URL 查询参数的实用方法。你可以使用它来获取 params 字符串，而不是创建复杂的字符串文本，如 ?page=1&query=a。
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    // 5.页面路由跳转，并携带参数
+    replace(`${pathname}?${params.toString()}`);
+  },300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
       <input
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
+          className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+          placeholder={placeholder}
+          onChange={(e) => {
+            handleSearch(e.target.value);
+          }}
+          // 同步搜索值
+          defaultValue={searchParams.get('query')?.toString()}
+
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
